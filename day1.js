32ab29ac0ab09a435c8275c400e3a8f1a4ed1f44
unknown <Minskoleopc>
2023-07-21 08:12:59 -0700

    added

---
+++ day1.js
@@ -0,0 +1,54 @@
+console.log('hello')
+
+// program 1
+
+let x = 10
+console.log(x)
+x = 900
+console.log(x)
+
+// program 2
+
+const  y = 1000
+console.log(y)
+//y = 8000
+
+
+// program 3
+
+
+let s = 10
+let t = 5
+
+console.log(s+t)
+console.log(s-t)
+console.log(s*t)
+console.log(s/t)
+console.log(s%t)
+
+let q = 8
+let r = 4
+
+console.log(q+r)
+console.log(q-r)
+console.log(q*r)
+console.log(q/r)
+console.log(q%r)
+
+
+// 7:30 pm &&  8:30am
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
