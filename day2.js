ebce9f4b6bd703bb17bf135c5f5fb084cfbcd373
unknown <Minskoleopc>
2023-07-22 06:23:11 -0700

    added

---
+++ day2.js
@@ -0,0 +1,74 @@
+// program 1
+let a = 10
+console.log(a)
+a = 100
+console.log(a)
+
+// program 2
+
+const h = 100
+console.log(h)
+//h = 78
+
+// program 3
+
+let s = 8
+let t = 4
+
+console.log(s+t)
+console.log(s-t)
+console.log(s*t)
+console.log(s/t)
+console.log(s%t)
+//console.log(23%5)
+
+// programm 4
+let r = 10
+let q = 5
+
+console.log(r+q)
+console.log(r-q)
+console.log(r*q)
+console.log(r/q)
+console.log(r%q)
+
+// 10 pair ------ 50 lines
+
+function Calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+Calculator(6,3)
+Calculator(8,4)
+
+// program 5
+
+// function without parameter and without return type
+
+function addA(){
+    console.log(8+8)
+}
+addA()
+addA()
+addA()
+addA()
+addA()
+
+// function with parameter and without return type 
+function addB(x,y){
+    console.log(x+y)
+}
+addB(12,4)
+addB(2,4)
+addB(3,4)
+
+// Monday - 8:30 am 
+// Monday - 7:30 pm
+
+
+
+
+
