425bcb9ccbcbdb2aea8c38410c5fbd6f7bf62666
unknown <Minskoleopc>
2023-07-25 20:29:21 -0700

    added

---
+++ day4E.js
@@ -0,0 +1,142 @@
+let x = 10
+console.log(x)
+x =  500
+console.log(x)
+
+// program 2
+const j = 100
+console.log(j)
+//j = 700
+console.log(j)
+
+
+// program 3
+
+
+let  a = 10
+let  b = 5
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+
+let s = 8
+let t = 4
+
+console.log(s+t)
+console.log(s-t)
+console.log(s*t)
+console.log(s/t)
+console.log(s%t)
+
+// program 4
+function Calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+Calculator(12,4)
+Calculator(6,3)
+Calculator(10,5)
+
+// program 5
+function addA(){
+    console.log(9+9)
+}
+addA()
+addA()
+addA()
+addA()
+addA()
+
+function addB(x,y){
+    console.log(x+y)
+}
+addB(12,5)
+
+function addC(x,y){
+    return x + y
+}
+
+let q1 = addC(12,4)
+console.log(q1)
+console.log(q1 + q1)
+
+// Type 
+
+// Extrovert                // Introvert 
+// more social              // less social
+// more outing              // less outing
+// loud                     // calm
+
+
+
+// Human 
+// Propeties - height , weight , color , gender
+// Method    - talk() , walk()
+
+// Vehicle - color , regNo , start() , stop()
+// Properties - regNo , color , model 
+// Method  - start() , stop()
+
+// Bank -
+// Properties - accNo , accName ,branchName
+// Method  - withdrawl() , deposit()
+
+
+let x1 = 10
+console.log(x1)
+console.log(typeof x1)
+// 0 , 12,-12,14.6,67.8
+
+let x2 = true
+console.log(x2)
+console.log(typeof x2)
+
+// true false
+let x3 = "chinmay"
+console.log(x3)
+// " " , '' , ``
+// "c" , "Ca1" , "Cs113##" , " " 
+
+// comparison operator 
+// entity < entity  ======> true or false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- day4M.js
+++ day4M.js
@@ -1,121 +1,82 @@
 let x = 10
 console.log(x)
-x =  500
-console.log(x)
+console.log(typeof x)
+// 12,-12,0,12.3,-12.6
 
-// program 2
-const j = 100
-console.log(j)
-//j = 700
-console.log(j)
+let y = true 
+console.log(y)
+console.log(typeof y)
+// true or false
 
-
-// program 3
-
-
-let  a = 10
-let  b = 5
-
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
-console.log(a%b)
-
-
-let s = 8
-let t = 4
-
-console.log(s+t)
-console.log(s-t)
-console.log(s*t)
-console.log(s/t)
-console.log(s%t)
-
-// program 4
-function Calculator(x,y){
-    console.log(x+y)
-    console.log(x-y)
-    console.log(x*y)
-    console.log(x/y)
-    console.log(x%y)
-}
-Calculator(12,4)
-Calculator(6,3)
-Calculator(10,5)
-
-// program 5
-function addA(){
-    console.log(9+9)
-}
-addA()
-addA()
-addA()
-addA()
-addA()
-
-function addB(x,y){
-    console.log(x+y)
-}
-addB(12,5)
-
-function addC(x,y){
-    return x + y
-}
-
-let q1 = addC(12,4)
-console.log(q1)
-console.log(q1 + q1)
-
-// Type 
-
-// Extrovert                // Introvert 
-// more social              // less social
-// more outing              // less outing
-// loud                     // calm
-
-
-
-// Human 
-// Propeties - height , weight , color , gender
-// Method    - talk() , walk()
-
-// Vehicle - color , regNo , start() , stop()
-// Properties - regNo , color , model 
-// Method  - start() , stop()
-
-// Bank -
-// Properties - accNo , accName ,branchName
-// Method  - withdrawl() , deposit()
-
-
-let x1 = 10
-console.log(x1)
-console.log(typeof x1)
-// 0 , 12,-12,14.6,67.8
-
-let x2 = true
-console.log(x2)
-console.log(typeof x2)
-
-// true false
-let x3 = "chinmay"
-console.log(x3)
-// " " , '' , ``
-// "c" , "Ca1" , "Cs113##" , " " 
+let firstName = "chinmay"
+console.log(firstName)
+console.log(typeof firstName)
+// '', "",``
+// "A", "a" , "1", "#", "123asA@" , " "
 
 // comparison operator 
-// entity < entity  ======> true or false
+// entity < entity =======> boolean
+
+console.log(typeof '10') // string
+console.log(typeof 10) // number
+
+// < , > , <= , >=
+// == , !=        -------> value
+// === , !==      -------> value and type 
+
+console.log(10 == '10')
+console.log(10 === '10')
+console.log(10 === 10)
+console.log(10 !== '10')
+console.log(10 != '10') 
+console.log(10 != 10)  
+console.log(10 !== 10) 
+console.log(8 > 4)
+console.log(8 < 4)
+console.log(8 >= 4)
+console.log(4 <= 8)
+console.log(4 >= 4)
+console.log(4 <= 4)
 
 
+//  Logical operator
 
 
+// AND --&&
+
+//true      &&    true    -------> true
+//false     &&    true    -------> false
+//true      &&    false   -------> false
+//false     &&    false   -------> false
+
+console.log(8 == 8 && 7 == 7)
+console.log(8 != 8 && 7 == 7)
+console.log(8 == 8 && 7 != 7)
+console.log(8 != 8 && 7 != 7)
 
 
+// OR -- ||
 
 
+//true      ||    true    -------> true
+//false     ||    true    -------> true
+//true      ||    false   -------> true
+//false     ||    false   -------> false
 
 
+console.log(8 == 8 || 7 == 7)
+console.log(8 != 8 || 7 == 7)
+console.log(8 == 8 || 7 != 7)
+console.log(8 != 8 || 7 != 7)
+
+
+// not 
+
+// true  ---> false
+// false ---> true
+
+console.log(!(7 == 7))
+console.log(!(7 != 7))
 
 
 
