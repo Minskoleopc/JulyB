cdb320fe5325da139c8aaa05ccc000a986e1cf00
unknown <Minskoleopc>
2023-07-27 07:26:41 -0700

    added

---
+++ day6E.js
@@ -0,0 +1,102 @@
+// conditional statements 
+
+// one input  ---- multiple outcome
+
+// ticket > 0 && ticket <= 5  ------ 10 % discount
+// ticket > 5 && ticket <= 10 ------ 20 % discount
+// ticket > 10                ------ 30 % discount
+
+// program 1
+let ticket = -17
+
+// if(ticket > 0 && ticket <= 5){
+//     console.log('10 % discount')
+// }
+// if(ticket > 5 && ticket <= 10){
+//     console.log('20 % discount')
+// }
+// if(ticket > 30){
+//     console.log('30 % discount')
+// }
+
+// program 2
+
+// if(ticket > 0 && ticket <= 5){
+//     console.log('10 % discount')
+// }
+// else if(ticket > 5 && ticket <= 10){
+//     console.log('20 % discount')
+// }
+// else if(ticket > 10){
+//     console.log('30 % discount')
+// }
+// else {
+//     console.log("incorrect value")
+// }
+
+// program 3
+
+let marks = 55
+
+// if(marks > 90){
+//     console.log('Grade A')
+// }
+// if(marks > 75){
+//     console.log('Grade B')
+// }
+// if(marks > 65){
+//     console.log('Grade C')
+// }
+
+// if(marks > 90){
+//     console.log('Grade A')
+// }
+// else if(marks > 75){
+//     console.log('Grade B')
+// }
+// else if(marks > 65){
+//     console.log('Grade C')
+// }
+// else {
+//     console.log('please try again')
+// }
+
+// program 4 
+
+
+let s = 8
+let t = 40
+
+if(s > t){
+    console.log("s is greater")
+}
+else {
+    console.log('t is greater')
+}
+
+// program 5
+
+let x = 10
+let y = 50
+let z = 300
+
+if(x > y && x > z){
+    console.log('x is greater')
+}
+else if(y > x && y > z){
+    console.log("y is greater")
+}
+else {
+    console.log("z is greater")
+}
+
+
+
+
+
+
+
+
+
+
+
