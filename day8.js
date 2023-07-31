add706cabcd7b2e52191cf1b7b84f98a378b50c8
unknown <Minskoleopc>
2023-07-31 07:22:45 -0700

    added

---
+++ day8.js
@@ -0,0 +1,49 @@
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+
+// for loop 
+//for(intialization ; conditionCheck ; increment/decrement){
+        // statement
+//}
+
+for(let i = 1 ; i <= 3 ; i++){  // 2  // 3 // 4
+    console.log(i)  // 1  // 2 // 3
+}
+
+
+// program 2
+for(let i = 1 ; i <= 3 ; i++){ // 2 // 3 // 4
+    console.log("hello") // hello // hello // hello
+}
+
+// 1 <= 3   ======= true  
+// 2 <= 3   ======  true
+// 3 <= 3   ======  true
+// 4 <= 3   ======  false
+
+
+// program 3
+for(let i = 5 ; i >= 1 ; i--){ // 4 // 3 // 2 // 1 // 0
+    console.log(i) // 5 // 4 // 3 // 2 //1
+}
+
+// program 4
+// 2   --------  20
+for(let i = 2 ; i <= 20 ; i = i + 2){  // 4  ------------- 22
+    console.log(i) // 2  // 4 ------------------20
+}
+
+// program 5 
+
+for(let i = 50 ; i >= 5 ; i = i - 5){ // 45 // 40 // 35  ------ 0
+    console.log(i) // 50 // 45 // 40 // 35 --------- 5
+}
+
+
+
+
+
