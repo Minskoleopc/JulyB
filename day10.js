8124bc1e6343a7d489e3f562fbdb760e433883ee
unknown <Minskoleopc>
2023-08-01 20:20:28 -0700

    added

---
+++ day10.js
@@ -0,0 +1,85 @@
+
+
+// loop 
+
+
+//for loop
+// for(intialization ; conditionCheck ; increment/decrement){
+//     //  statement
+// }
+
+// program 1
+for (let i = 1; i <= 3; i++) { // 2 // 3 // 4
+    console.log(i) // 1 // 2 // 3
+}
+
+// program 2
+
+for (let i = 1; i <= 3; i++) { // 2 // 3 // 4
+
+    console.log("hello") // hello // hello // hello
+
+}
+//  1 <= 3 ---> true
+//  2 <= 3 ---> true
+//  3 <= 3 ---> true 
+
+// program to print 0 to 5
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i)
+}
+
+// program print  5 to 1
+
+for (let i = 5; i >= 1; i--) { // 4 // 3 // 2 // 1 // 0
+    console.log(i) // 5 // 4 // 3 // 2 // 1
+}
+
+// program to print table of two
+for (let i = 2; i <= 20; i = i + 2) { // 4 // 6 // 8 ----- 22
+    console.log(i) // 2 // 4 // 6 ---------------20
+}
+// table of 5 in reverse 
+
+for(let i = 50 ; i >= 5 ; i = i - 5){ // 45 // 40 ----- 0
+    console.log(i) // 50 // 45  --------------- 5 
+}
+
+
+// break with for 
+// continue with for
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// while loop
+
+
