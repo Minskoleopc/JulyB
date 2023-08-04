d78da8215cba3ed1cc335bff773c018147aa6463
unknown <Minskoleopc>
2023-08-04 07:23:48 -0700

    added

---
+++ day14.js
@@ -0,0 +1,64 @@
+let x = 10
+console.log(x)
+
+//             0         1        2      3      4
+let names = ["mayuri","sarika","komal","ram","sham"]
+console.log(names[0])
+console.log(names[3])
+
+// Human ----- rahul
+// Properties - age , weight , height , gender 
+// Method    -  talk(), walk()
+
+let q1 = names.length
+console.log(q1)
+
+
+// Gym 
+// action ----> excercise
+// return ----> health
+
+// push()
+
+let cities = ["pune","banglore","ujjain"]
+let q2 = cities.push("nagpur")
+console.log(q2)
+console.log(cities)
+
+// unshift()
+let fruits = ["apple","mango","banana","grapes"]
+let q3 = fruits.unshift("chikoo")
+console.log(q3)
+console.log(fruits)
+
+// pop()
+let vegetables = ["tomato","potato","brinjal","cabbage"]
+let q4 = vegetables.pop()
+console.log(q4)
+console.log(vegetables)
+
+// shif()
+let flowers = ["lily","lotus","rose","sunflower"]
+let q5 = flowers.shift()
+console.log(q5)
+console.log(flowers)
+
+// includes()
+
+let countries = ["india","pakistan","srilanka","japan"]
+let q6 = countries.includes('Pakistan')
+console.log(q6)
+
+// length 
+// push()  --- new length
+// unshift() --- new length
+// pop()   ---- lastElement
+// shift()  ---- firstName
+// includes()  --- boolean value 
+
+
+
+
+
+
+
